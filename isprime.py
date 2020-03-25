67c2b1ab469ad2cc927a2e4e12208303bca37401
Qixiaomao <xdedm1@163.com>
2020-03-25 19:23:43 +0800

    Isprme

---
+++ isprime.py
@@ -0,0 +1,15 @@
+def isprime():
+    x = True
+    y = False
+    e = int(raw_input('Enter a num:'))
+    if e%2 == 0 :
+        print x
+    else:
+        print y
+      
+    
+
+def main():
+    isprime()
+
+main()
